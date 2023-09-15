8f5770c84d9b09b978b3c9eaf792da712af7aab8
777dan <danya.nikitin7@gmail.com>
2023-09-15 20:46:47 +0300

    the song was modified

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const bottlesCount = prompt("How many bottles would like?");
 const drink = prompt("And which drink would you prefer?");
 let bottles = bottlesCount;
-for (let i = 1; i <= bottlesCount; i++) {
-    alert(`${bottles} bottles of ${drink} on the wall,`);
-    alert(`${bottles} bottles of ${drink},`);
-    alert("If one of those bottles should happen to fall");
-    bottles -= 1;
+for (let i = 0; i <= bottlesCount; i++) {
+    if (bottles !== 0) {
+        alert(`${bottles} bottles of ${drink} on the wall,`);
+        alert(`${bottles} bottles of ${drink},`);
+        alert("If one of those bottles should happen to fall");
+        bottles -= 1;
+    } else {
+        alert(`No more bottles of ${drink} on the wall, no more bottles of ${drink}.`);
+        alert(`There's nothing else to fall, because there's no more bottles of ${drink} on the wall.`);
+    }
 }
