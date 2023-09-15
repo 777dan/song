42703b590be0ec44aaff7336127cb5da261d944c
777dan <danya.nikitin7@gmail.com>
2023-09-15 20:21:39 +0300

    added some feature

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const bottlesCount = prompt("How many bottles of beer would like?");
+const bottlesCount = prompt("How many bottles would like?");
+const drink = prompt("And which drink would you prefer?");
 let bottles = bottlesCount;
 for (let i = 1; i <= bottlesCount; i++) {
-    alert(`${bottles} bottles of beer on the wall,`);
-    alert(`${bottles} bottles of beer,`);
+    alert(`${bottles} bottles of ${drink} on the wall,`);
+    alert(`${bottles} bottles of ${drink},`);
     alert("If one of those bottles should happen to fall");
     bottles -= 1;
 }
